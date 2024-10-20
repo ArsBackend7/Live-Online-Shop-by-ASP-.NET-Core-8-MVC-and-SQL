03577502121eff94450bc25709000c5e62858b52
Miftaul_Islam_Ariyan <124516497+MiftaulIslam@users.noreply.github.com>
2024-10-20 23:11:26 +0600

    LOS-4: Models added

---
+++ Project/Web/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Web.Data;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(
+    options => options.UseSqlite(builder.Configuration.GetConnectionString("DCS")));
+builder.Services.AddControllersWithViews();
+
+WebApplication app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    _ = app.UseExceptionHandler("/Home/Error");
+    _ = app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+await app.RunAsync();
